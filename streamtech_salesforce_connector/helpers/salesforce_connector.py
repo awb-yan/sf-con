da8380929bad5ce03dcd0949f4b06bedd4621601
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-07-19 15:46:14 +0800

    remove logger in the helper file

--- streamtech_salesforce_connector/helpers/salesforce_connector.py
+++ streamtech_salesforce_connector/helpers/salesforce_connector.py
@@ -2,15 +2,16 @@ from odoo import models, fields, api, exceptions, _
 from simple_salesforce import Salesforce
 from openerp.exceptions import Warning
 
+_logger = logging.getLogger(__name__)
+
 class SalesForceConnect(object):
     def connect_salesforce(self, model):
         IrConfigParameter = model.env['ir.config_parameter'].sudo()
+        username = IrConfigParameter.get_param('odoo_salesforce.sf_username')
+        password = IrConfigParameter.get_param('odoo_salesforce.sf_password')
+        security = IrConfigParameter.get_param('odoo_salesforce.sf_security_token')
+        domain = IrConfigParameter.get_param('odoo_salesforce.sf_domain')
 
-        _logger.info("Connect to salesforce")
-        _logger.info("username: [%s]" % username)
-        _logger.info("password: [%s]" % password)
-        _logger.info("security: [%s]" % security)
-        _logger.info("domain: [%s]" % domain)
         sales_force = Salesforce(username=username, password=password, security_token=security, domain=domain)
 
         return sales_force
