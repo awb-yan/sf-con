c4646ad182c8fb274284010a7f2216bbee224abc
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-07-19 12:54:20 +0800

    removed try-catch

--- streamtech_salesforce_connector/helpers/salesforce_connector.py
+++ streamtech_salesforce_connector/helpers/salesforce_connector.py
@@ -7,23 +7,20 @@ _logger = logging.getLogger(__name__)
 
 class SalesForceConnect(object):
     def connect_salesforce(self):
-        try:
-            IrConfigParameter = self.env['ir.config_parameter'].sudo()
-            username = IrConfigParameter.get_param('odoo_salesforce.sf_username')
-            password = IrConfigParameter.get_param('odoo_salesforce.sf_password')
-            security = IrConfigParameter.get_param('odoo_salesforce.sf_security_token')
-            domain = IrConfigParameter.get_param('odoo_salesforce.sf_domain')
+        IrConfigParameter = self.env['ir.config_parameter'].sudo()
+        username = IrConfigParameter.get_param('odoo_salesforce.sf_username')
+        password = IrConfigParameter.get_param('odoo_salesforce.sf_password')
+        security = IrConfigParameter.get_param('odoo_salesforce.sf_security_token')
+        domain = IrConfigParameter.get_param('odoo_salesforce.sf_domain')
 
-            _logger.info("Connect to salesforce")
-            _logger.info("username: [%s]" % username)
-            _logger.info("password: [%s]" % password)
-            _logger.info("security: [%s]" % security)
-            _logger.info("domain: [%s]" % domain)
-            sales_force = Salesforce(username=username, password=password, security_token=security, domain=domain)
-            _logger.info("SF: [%s]" % sales_force)
+        _logger.info("Connect to salesforce")
+        _logger.info("username: [%s]" % username)
+        _logger.info("password: [%s]" % password)
+        _logger.info("security: [%s]" % security)
+        _logger.info("domain: [%s]" % domain)
+        sales_force = Salesforce(username=username, password=password, security_token=security, domain=domain)
+        _logger.info("SF: [%s]" % sales_force)
 
-            return sales_force
-        except Exception as e:
-            Warning(_(str(e)))
+        return sales_force
 
 
