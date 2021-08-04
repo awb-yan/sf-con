164dfabcedbc2e3635e46adb0dc5d2d1af6f3b89
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-08-04 13:52:23 +0800

    testing

---
+++ awb_odoo_rest_api/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
+{
+    'name': "AWB Odoo Rest API",
+    'summary': """
+        AWB Odoo Rest API
+        """,
+    'description': """
+        AWB Odoo Rest API
+    """,
+    'author': "Achieve Without Borders, Inc",
+    'website': "http://www.achievewithoutborders.com",
+    'category': 'Sales',
+    'version': '13.0.1.11.0',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
